2dbfbada5bfb5783f240ab190ea949c2419e81a7
lvxinyun@snsoft.com.cn <lvxinyun@snsoft.com.cn>
2021-07-08 18:20:37 +0800

    增加SQL表结构

---
+++ eastnight-config/src/test/java/cn/eastnight/eastnightconfig/EastnightConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.eastnight.eastnightconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EastnightConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
